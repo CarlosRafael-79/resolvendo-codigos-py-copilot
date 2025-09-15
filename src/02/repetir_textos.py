d27877c41b5054184e370cd671e86238d3750753
Carlos Rafael <94875793+CarlosRafael-79@users.noreply.github.com>
2025-09-15 22:52:09 +0000

    feat: Resolvendo Desafio
    Foi implementado os 6 desafios sugeridos para o projeto.

---
+++ src/02/repetir_textos.py
@@ -0,0 +1,9 @@
+# Solicita uma string do usuário
+texto = input("Digite o texto a ser repetido: ")
+
+# Solicita um número inteiro
+vezes = int(input("Digite o número de repetições: "))
+
+# Repete e exibe o texto
+resultado = texto * vezes
+print("Resultado:", resultado)
